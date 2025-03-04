3e7c83605a1e558ecf2c36cdb7d8d4965de6da6d
Jutin Joco <justin.joco@gmail.com>
2025-03-04 01:19:43 -0800

    adds postgres connection

--- backend/main.go
+++ backend/main.go
@@ -6,28 +6,40 @@ import (
 	"app/service"
 	"database/sql"
 	"fmt"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	_ "github.com/lib/pq"
 )
 
 func main() {
 	r := gin.Default()
 
-	connStr := ""
+	connStr := "postgres://admin:password@postgres/app_db"
 	db, err := sql.Open("postgres", connStr)
 	if err != nil {
 		panic(fmt.Sprintf("Connection went wrong: %s", err.Error()))
 	}
+	defer db.Close()
+
+	maxAttempts := 3
+	for i := 0; i < maxAttempts; i++ {
+		err := db.Ping()
+		if err == nil {
+			break
+		}
+		time.Sleep(1 * time.Second)
+	}
 
 	repo := repository.NewBookRepository(db)
-	service := service.NewBookService(repo)
-	controller := controller.NewBookController(service)
+	svc := service.NewBookService(repo)
+	ctr := controller.NewBookController(svc)
 
-	r.GET("/books", controller.ListBooks)
-	r.GET("/book/:id", controller.GetBookById)
-	r.POST("/book", controller.CreateBook)
-	r.PUT("/book/:id", controller.UpdateBookById)
-	r.DELETE("/book/:id", controller.DeleteBookById)
+	r.GET("/books", ctr.ListBooks)
+	r.GET("/book/:id", ctr.GetBookById)
+	r.POST("/book", ctr.CreateBook)
+	r.PUT("/book/:id", ctr.UpdateBookById)
+	r.DELETE("/book/:id", ctr.DeleteBookById)
 
 	r.Run(":5000")
 }
