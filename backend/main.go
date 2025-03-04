e9e1ee1a5224fc25aef52d466b8c1e98ba35179b
Jutin Joco <justin.joco@gmail.com>
2025-03-04 01:35:41 -0800

    adds reconnect logic

--- backend/main.go
+++ backend/main.go
@@ -5,31 +5,47 @@ import (
 	"app/repository"
 	"app/service"
 	"database/sql"
-	"fmt"
+	"log"
 	"time"
 
 	"github.com/gin-gonic/gin"
 	_ "github.com/lib/pq"
 )
 
-func main() {
-	r := gin.Default()
+func connectToDB() (*sql.DB, error) {
 
 	connStr := "postgres://admin:password@postgres/app_db"
 	db, err := sql.Open("postgres", connStr)
 	if err != nil {
-		panic(fmt.Sprintf("Connection went wrong: %s", err.Error()))
+		log.Fatal("DB connection failed")
 	}
-	defer db.Close()
 
-	maxAttempts := 3
-	for i := 0; i < maxAttempts; i++ {
-		err := db.Ping()
+	err = db.Ping()
+	if err == nil {
+		db.Close()
+		return nil, err
+	}
+
+	return db, nil
+}
+
+func main() {
+	r := gin.Default()
+
+	var db *sql.DB
+	var err error
+
+	for {
+		db, err = connectToDB()
 		if err == nil {
+			log.Println("Connection success")
 			break
 		}
+		log.Println("Waiting to connect again")
 		time.Sleep(1 * time.Second)
 	}
+
+	defer db.Close()
 
 	repo := repository.NewBookRepository(db)
 	svc := service.NewBookService(repo)
